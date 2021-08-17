a4f3a17c8ddc55b2ae81ea024a63815314ce2b4e
Adithya09 <ap2g20@soton.ac.uk>
2021-08-17 16:55:05 +0100

    All internship requirement functionalities added

--- index.js
+++ index.js
@@ -4,33 +4,11 @@ let map;
 //Defining a global drawingManager object
 let drawingManager;
 
-//Defining a global marker object
-let marker;
-
-//Defining a global newShape
-let newShape;
-
-//Defining a shape that has been selected
-let selectedShape;
-
-//Defining an array of polygons
-let polygonArray = [];
-
-//Function to convert an object to a string
-function objToString (obj) {
-    var str = '';
-    for (var p in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, p)) {
-            str += p + '::' + obj[p] + '\n';
-          }
-        }
-    return str;
-}
-
-//Function to generate a random code for each polygon
+//Function to generate a random 5 character ID for each polygon
 function makeID(){
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  var characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
   for ( var i = 0; i < 5; i++ ) {
     result += characters.charAt(Math.floor(Math.random() *
@@ -39,6 +17,16 @@ function makeID(){
   return result;
 }
 
+//Function to refresh the map
+function refresh(){
+  initMap();
+  document.getElementById('msg').innerHTML = "";
+}
+
+/*
+  Initialising the map, centered at London with drawing manager tools and
+  distance calculation tools
+*/
 function initMap(){
   //Creating a new map with the center in London
    map = new google.maps.Map(document.getElementById("map"), {
@@ -70,61 +58,70 @@ function initMap(){
   });
   drawingManager.setMap(map);
 
-  /*Adding functionality to check if a clicked point lies inside a polygon or
+  /*
+    Adding functionality to check if a clicked point lies inside a polygon or
     outside
   */
   google.maps.event.addListener(map, "click", function(E){
- alert(E.latLng + " does not lie inside a polygon");
- });
- google.maps.event.addListener(drawingManager, "overlaycomplete", function(e){
-   var coordinatesArray = e.overlay.getPath().getArray();
-   coordinatesArray = makeID();
-   google.maps.event.addListener(e.overlay, "click", function(event){
-     console.log(event.latlng);
-     if(google.maps.geometry.poly.containsLocation(event.latLng, e.overlay)){
-     alert(event.latLng + " lies inside the polygon: " + coordinatesArray);
-   }
- });
+    alert(E.latLng + " does not lie inside a polygon");
+  });
+  google.maps.event.addListener(drawingManager, "overlaycomplete", function(e){
+    var id = e.overlay.getPath().getArray();
+    id = makeID();
+    google.maps.event.addListener(e.overlay, "click", function(event){
+      if(google.maps.geometry.poly.containsLocation(event.latLng, e.overlay)){
+        alert(event.latLng + " lies inside the polygon: " + id);
+      }
+    });
  });
 
  /*
   Calculating the distance between two points on the Map
  */
- //Adding a marker on the first set of coordinates
- const marker1 = document.getElementById("marker1").value;
- const latlngStr1 = marker1.split(",", 2);
- var lat1 = parseFloat(latlngStr1[0]);
- var lng1 = parseFloat(latlngStr1[1])
- var marker1position = new google.maps.LatLng(lat1, lng1);
- var mk1 = new google.maps.Marker({position: marker1position, map: map});
-
- //Adding a marker on the second set of coordinates
- const marker2 = document.getElementById("marker2").value;
- const latlngStr2 = marker2.split(",", 2);
- var lat2 = parseFloat(latlngStr2[0]);
- var lng2 = parseFloat(latlngStr2[1]);
- var marker2position = new google.maps.LatLng(lat2, lng2);
- var mk2 = new google.maps.Marker({position: marker2position, map: map});
-
  document.getElementById("submit").addEventListener("click", () => {
-  let directionsService = new google.maps.DirectionsService();
-  let directionsRenderer = new google.maps.DirectionsRenderer();
-  directionsRenderer.setMap(map); // Existing map object displays directions
-  // Create route from existing points used for markers
-  const route = {
+   //Getting the coordinates of point A and placing a marker at these coords
+   const marker1 = document.getElementById("marker1").value;
+   const latlngStr1 = marker1.split(",", 2);
+   var lat1 = parseFloat(latlngStr1[0]);
+   var lng1 = parseFloat(latlngStr1[1])
+   var marker1position = new google.maps.LatLng(lat1, lng1);
+   var mk1 = new google.maps.Marker({position: marker1position, map: map});
+
+   //Getting the coordinates of point B and placing a marker at these coords
+   const marker2 = document.getElementById("marker2").value;
+   const latlngStr2 = marker2.split(",", 2);
+   var lat2 = parseFloat(latlngStr2[0]);
+   var lng2 = parseFloat(latlngStr2[1]);
+   var marker2position = new google.maps.LatLng(lat2, lng2);
+   var mk2 = new google.maps.Marker({position: marker2position, map: map});
+
+   /*
+   Utilising the google maps directions API to calculate the distance from
+   marker A to marker B
+   */
+   let directionsService = new google.maps.DirectionsService();
+   let directionsRenderer = new google.maps.DirectionsRenderer();
+
+   //Existing map object displays directions
+   directionsRenderer.setMap(map);
+
+   // Create route from existing points used for markers
+   const route = {
       origin: marker1position,
       destination: marker2position,
       travelMode: 'DRIVING'
-  }
+    }
 
-  directionsService.route(route,
-    function(response, status) { // anonymous function to capture directions
+   //Anonymous function to capture directions
+   directionsService.route(route,
+    function(response, status) {
       if (status !== 'OK') {
         window.alert('Directions request failed due to ' + status);
         return;
       } else {
         // Add route to the map
         directionsRenderer.setDirections(response);
+
         // Get data about the mapped routes
         var directionsData = response.routes[0].legs[0];
         if (!directionsData) {
@@ -132,12 +129,17 @@ function initMap(){
           return;
         }
         else {
-          alert(" Driving distance is " + directionsData.distance.text + " (" + directionsData.duration.text + ").")
-
-          document.getElementById('msg').innerHTML += " Driving distance is "
-          + directionsData.distance.text + " (" + directionsData.duration.text + ").";
+          document.getElementById('msg').innerHTML += "Driving distance and duration from "
+          + marker1position + " to" + marker2position + "is "
+          + directionsData.distance.text + " (" + directionsData.duration.text
+          + ").";
         }
       }
     });
  });
+
+  //Refresh the map if the "Refresh" button is clicked
+  document.getElementById("refresh").addEventListener("click", () => {
+    refresh();
+  });
 }
